fe341bf891a94187a391b0330009f291d3b25afd
Gregor Hueltenschmidt <gregor.hueltenschmidt@web.de>
2020-10-20 02:10:01 +0200

    index.xhtml, 4th part

--- src/main/java/com/gregorhue/theblog/controller/PostController.java
+++ src/main/java/com/gregorhue/theblog/controller/PostController.java
@@ -3,9 +3,6 @@ package com.gregorhue.theblog.controller;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-import javax.faces.context.FacesContext;
-import javax.faces.event.PhaseId;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
